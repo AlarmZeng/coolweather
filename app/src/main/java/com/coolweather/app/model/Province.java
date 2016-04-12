ecb86109c06272b1882d510c8f24f93eb7b1c830
AlarmZ <AlarmZ@163.com>
2016-04-12 20:28:04 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,37 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by hunter_zeng on 2016/4/12.
+ */
+public class Province {
+
+    private String provinceName;
+
+    private String provinceCode;
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
